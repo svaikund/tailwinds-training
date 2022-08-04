b1612706b625c16d664eaabec20b37e63404b37d
srinivasan v <svaikund@yahoo.com>
2022-08-04 09:16:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+   response.end('Hello World \n');
+}).listen(8082);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
